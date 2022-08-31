9b7020f162601cf22a8ba44a7542ee64f01ffa94
Lukesudom <lukemarshall511@gmail.com>
2022-08-31 20:36:16 +1000

    finished routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,20 @@
+//Requirements
+
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async(req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.userId,
+        });
+        res.json(newComment);
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
